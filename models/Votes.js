a53a15fd3eb6ca194afb1dd221c42d8e20161699
Oskar Niburski <oskarniburski@gmail.com>
2014-10-16 23:51:14 -0400

    Deletion of files. Move to config mainly. Unable to properly make home.html work, so trying new approach

--- models/Votes.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose')
-
-var VoteSchema = new mongoose.Schema({
-  title: String,
-  selections: [{type: mongoose.Schema.Types.ObjectId, ref: 'Selection'}]  
-})
-
-mongoose.model('Vote', VoteSchema)
