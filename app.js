3fee7e4dd73fe050f800df6ec6b8272528b06d9c
Oskar Niburski <oskarniburski@gmail.com>
2014-10-16 23:50:36 -0400

    update of app.js to use better config files

--- app.js
+++ app.js
@@ -1,62 +1,21 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose')
-
-mongoose.connect('mongodb://localhost/votes')
-require('./models/Votes')
-require('./models/Selections')
-
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+var config = require('./server/config/environments')[env];
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// Express config
+require('./server/config/express')(app, config);
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// MongoDB
+var mongoose = require('mongoose');
+mongoose.connect(config.database);
+require('./server/models/Votes');
+require('./server/models/Selections');
 
-// var routes = require('./routes/index');
-// app.use('/', routes);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+// Routes
+require('./server/config/routes')(app);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
 });
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
-
-module.exports = app;
