47c461933e76ab7b3131ef8fba667df10cdd00d6
Oskar Niburski <oskarniburski@gmail.com>
2014-10-20 00:01:15 -0400

    Quick usage of socket.io from rchat

--- server.js
+++ server.js
@@ -18,3 +18,9 @@ require('./server/config/routes')(app);
 var server = app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + server.address().port);
 });
+
+var io = require('socket.io').listen(server)
+
+io.sockets.on('connection', function(socket) {
+    console.log('USER CONNECTED CHECK THIS OUT MA BOYYYYYZ')
+})
