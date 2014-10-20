b8a762f16331e5e2e9f1cc306e8d097960d9b104
Oskar Niburski <oskarniburski@gmail.com>
2014-10-19 23:37:19 -0400

    Footer scope

--- public/javascripts/controllers/controllers.js
+++ public/javascripts/controllers/controllers.js
@@ -1,10 +1,12 @@
 
 app.controller('VoteCtrl', ['votes', '$scope', '$stateParams', function(votes, $scope, $stateParams) {
   $scope.vote = votes.votes[$stateParams.title]
+  $scope.footer = true;
 }])
 
 app.controller('VotesCtrl', ['Vote','Selection','$scope', function(Vote, Selection, $scope) {
   $scope.votes = Vote.votes
+  $scope.footer = true;
 
   $scope.upvote = function(vote,selection) {
     console.log('test')
@@ -15,6 +17,7 @@ app.controller('VotesCtrl', ['Vote','Selection','$scope', function(Vote, Selecti
 app.controller('NewVotesCtrl', function(Vote, $scope) {
   $scope.votes = Vote.votes
   $scope.selections = []
+  $scope.footer = true;
 
   $scope.addVote = function() {
     if ( $scope.title === '' ) {
@@ -44,6 +47,8 @@ app.controller('NewVotesCtrl', function(Vote, $scope) {
 
 app.controller('MainCtrl', function($scope, $location, Vote) {
   $scope.votes = Vote.getAll()
+
+  $scope.footer = false;
 
   $scope.go = function( path ) {
     $location.path( path )
