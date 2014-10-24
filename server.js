53403abff118b8ff0c56337ae8d0f4ef6cacbb28
Oskar Niburski <oskarniburski@gmail.com>
2014-10-23 20:57:31 -0400

    removal of passwordless code

--- server.js
+++ server.js
@@ -7,12 +7,6 @@ var cookieParser = require('cookie-parser');
 var expressSession = require('express-session');
 var bodyParser = require('body-parser');
 
-var passwordless = require('passwordless');
-//var passwordless = require('../../');
-
-var MongoStore = require('passwordless-mongostore');
-var email   = require("emailjs");
-
 // Express config
 require('./server/config/express')(app, config);
 
@@ -28,34 +22,6 @@ var options = {
   db: 2,
   pass: 'RedisPASS'
 }
-
-var smtpServer = email.server.connect({
-  user: 'jacobzrobin@gmail.com',
-  password: '052014230xX!!!',
-  host: 'smtp.gmail.com',
-  ssl: true
-})
-
-var mongoURI = config.database
-
-passwordless.init(new MongoStore(mongoURI));
-passwordless.addDelivery(
-  function(tokenToSend, uidToSend, recipient, callback) {
-    var host = 'localhost:3000';
-    smtpServer.send({
-      text:    'Hello!\nAccess your account here: http://'
-      + host + '?token=' + tokenToSend + '&amp;uid='
-      + encodeURIComponent(uidToSend),
-      from:    'jacobzrobin@gmail.com',
-      to:      recipient,
-      subject: 'Token for ' + host
-    }, function(err, message) {
-      if(err) {
-          console.log(err);
-      }
-      callback(err);
-    });
-});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -73,35 +39,6 @@ app.use(function(req, res, next) {
   }
 })
 
-app.use(passwordless.sessionSupport());
-app.use(passwordless.acceptToken({ successRedirect: '/'}));
-
-app.get('/login', function(req, res) {
-  res.render('login');
-});
-
-app.post('/sendtoken', 
-  passwordless.requestToken(
-    // Simply accept every user
-    function(user, delivery, callback) {
-      callback(null, user);
-      // usually you would want something like:
-      // User.find({email: user}, callback(ret) {
-      //    if(ret)
-      //      callback(null, ret.id)
-      //    else
-      //      callback(null, null)
-      // })
-    }),
-  function(req, res) {
-      res.render('sent');
-  }
-);
-
-app.get('/restricted', passwordless.restricted(),
-    function(req, res) {
-        res.render('restricted', { user: req.user });
-});
 
 // Routes
 var routes = require('./server/config/routes')(app);
